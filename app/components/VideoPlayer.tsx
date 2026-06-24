928af5c465ecd2bababf8750c4e873893c061de0
Max Lair <max_lair@reyrey.com>
2026-06-24 07:42:41 -0400

    Video source fix

--- app/components/VideoPlayer.tsx
+++ app/components/VideoPlayer.tsx
@@ -6,8 +6,8 @@ const VideoPlayer = () => {
   return (
     <video className="h-full w-full rounded-lg" poster="/poster.png" controls>
       <source
-        src="/copy_E2384F24-E88C-4280-AAA3-071CB308FEB6.mov"
-        type="video/mov"
+        src="/hh_video.mp4"
+        type="video/mp4"
       />
       Your browser does not support the video tag.
     </video>
